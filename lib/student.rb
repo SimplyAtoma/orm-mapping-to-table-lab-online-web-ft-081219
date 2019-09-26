def7be36a417a5bcac440529ea07e05243fc30d6
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-09-26 22:49:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,14 +1,13 @@
 class Student
-
+attr_accessor :name , :grade
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   def self.create_table
     sql =  <<-SQL 
       CREATE TABLE IF NOT EXISTS songs (
         id INTEGER PRIMARY KEY, 
-        name TEXT, 
-        album TEXT
-        )
+        name TEXT,
+        grade INTEGER)
         SQL
     DB[:conn].execute(sql) 
   end
