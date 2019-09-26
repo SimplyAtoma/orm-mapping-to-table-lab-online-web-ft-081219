7b1305ca17a62e56a6d2ff056ce382014957021f
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-09-26 23:04:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,7 +30,7 @@ class Student
       VALUES (?, ?)
     SQL
  
-    DB[:conn].execute(sql, self.name, self.grade, id)
+    DB[:conn].execute(sql, self.name, self.grade)
   end 
   
   def self.create 
